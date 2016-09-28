a17bb7166b0dce1c1701ddd9c9969afe3a36202a
Brian Otte <brianotte@Brians-MacBook-Pro-2.local>
2016-09-28 12:22:25 -0600

    first interaction with a server

--- controller.js
+++ controller.js
@@ -1,7 +1,10 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	mainService.getUsers().then(function(response){
+      console.log(response);
+      $scope.users = response.data.data;
+    });
   }
 
 
